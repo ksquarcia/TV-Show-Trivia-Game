59c813dd8db13cd13a859e33621cb79f0f54f57a
Kyle Squarcia <ksquarcia2008@gmail.com>
2019-03-01 08:30:38 -0800

    added the questions to a seperate javascript page

---
+++ assets/javascript/questions.js
@@ -0,0 +1,91 @@
+var questions = [{
+    question: 'Who played Chandler\'s father on Friends?',
+    answers: [
+        'Nathan Lane',
+        'Michael Douglas',
+        'Kathleen Turner',
+        'Elliot Gould'
+    ],
+    correctAnswer: 2
+}, {
+    question: 'In Bewitched, what part of her body does Samantha twitch to practice magic?',
+    answers: [
+        'Her ears',
+        'Her botom',
+        'Her nose',
+        'Her forehead'
+    ],
+    correctAnswer: 2
+}, {
+    question: 'Every episode of Seinfeld contains an image or reference to what superhero?',
+    answers: [
+        'Superman',
+        'Plastic man',
+        'Batman',
+        'Spiderman'
+    ],
+    correctAnswer: 3
+}, {
+    question: 'What TV series showed the first interracial kiss on American network television?',
+    answers: [
+        'All in the Family',
+        'Family Feud',
+        'The Cosby Show',
+        'Star Trek'
+    ],
+    correctAnswer: 3
+}, {
+    question: 'What is the phrase on the UFO poster in Fox Mulders office in X-Files?',
+    answers: [
+        'I want to Belive',
+        'The Truth is Out There',
+        'Trust No One',
+        'We are Not Alone'
+    ],
+    correctAnswer: 0
+}, {
+    question: 'What television series was the first to air the sound of a toilet being flushed?',
+    answers: [
+        'The Addams Family',
+        'Leave it to Beaver',
+        'Maude',
+        'All in the Family'
+    ],
+    correctAnswer: 3
+}, {
+    question: 'Who shot J.R.?',
+    answers: [
+        'Sue Ellen Ewing',
+        'Pamela Barnes Ewing',
+        'Kristin Shepard',
+        'Jenna Wade'
+    ],
+    correctAnswer: 2
+}, {
+    question: 'What color was Jeannie\'s harem costume in I Dream of Jeannie?',
+    answers: [
+        'Pink',
+        'Blue',
+        'Green',
+        'Yellow'
+    ],
+    correctAnswer: 0
+}, {
+    question: 'In what TV show did Sheriff Andy Taylor first appear?',
+    answers: [
+        'Gomer Pyle: USMC',
+        'Mayberry R.F.D.',
+        'The Danny Thomas Show',
+        'The Andy Griffith Show'
+    ],
+    correctAnswer: 2
+}, {
+    question: 'What sort of business did George Jefferson own on The Jeffersons?',
+    answers: [
+        'Butcher shop',
+        'Dry cleaner',
+        'Construction company',
+        'Gas station'
+    ],
+    correctAnswer: 1
+}];
